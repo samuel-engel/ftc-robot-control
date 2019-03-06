968bf7952580ede537221860e2680b0000a83fab
Samuel Engel <samuel-engel@outlook.com>
2019-03-06 22:13:53 +0100

    refactor how robot centers on a gold mineral
    add documentation

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptTensorFlowObjectDetection.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptTensorFlowObjectDetection.java
@@ -31,6 +31,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
@@ -59,8 +60,11 @@ public class ConceptTensorFlowObjectDetection extends LinearOpMode {
     private static final String VUFORIA_KEY = "AZVtCBn/////AAABmc0ZBKXorUd9jx9puP8gcV96rgljk0hDKL2staD0PinAX8J8c8P/UubaAwj+waF8pY3SKAGGxDh7ZfSCgr30RKxBJ/UfJkUmCbY3q8OhOGkwaSWrNk4A/BR5Sfzbw/VFRofB9n0e9jYBCJe4Rxm2kcOKa0VhER/r7VEgI2ZUhGN58BQN6ZY8j7+QUHlLTFsMm9IOAyqOc1C4QPHc5/T0tCG/mKbXOsY6l7mI6XqjUB/UmBl7I+1VhPR3hsoHJelnGqkp+uW5BqMdWwIaz7wd5D0v6Y3ZW33MjN348C32rAlwP4D4LPbI1OqSBFtS544AjbK97zojViEy1i534ykZs5MjvJYXVGiHgDxUSeMYGzOt";
 
     double  power_center;
+    DcMotor motor_center;
 
     int left_coordinate, screen_width, width, left_boundary, distance_to_boundary;
+    String gold_mineral_position = null;
+
 
     /**
      * {@link #vuforia} is the variable we will use to store our instance of the Vuforia
@@ -78,6 +82,8 @@ public class ConceptTensorFlowObjectDetection extends LinearOpMode {
     public void runOpMode() {
         // The TFObjectDetector uses the camera frames from the VuforiaLocalizer, so we create that
         // first.
+        motor_center = hardwareMap.get(DcMotor.class, "center_drive");
+
         initVuforia();
 
         if (ClassFactory.getInstance().canCreateTFObjectDetector()) {
@@ -102,55 +108,88 @@ public class ConceptTensorFlowObjectDetection extends LinearOpMode {
                     // getUpdatedRecognitions() will return null if no new information is available since
                     // the last time that call was made.
                     List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+
+                    //if detecting something
                     if (updatedRecognitions != null) {
                         telemetry.addData("# Object Detected", updatedRecognitions.size());
-                        if (updatedRecognitions.size() == 1) {
 
+
+                        // if three objects detected, find the position of the golden mineral
+                        if (updatedRecognitions.size() == 3 && gold_mineral_position == null) {
+                            int goldMineralX = -1;
+                            int silverMineral1X = -1;
+                            int silverMineral2X = -1;
                             for (Recognition recognition : updatedRecognitions) {
+                                if (recognition.getLabel().equals(LABEL_GOLD_MINERAL)) {
+                                    goldMineralX = (int) recognition.getLeft();
+                                } else if (silverMineral1X == -1) {
+                                    silverMineral1X = (int) recognition.getLeft();
+                                } else {
+                                    silverMineral2X = (int) recognition.getLeft();
+                                }
+                            }
+                            if (goldMineralX != -1 && silverMineral1X != -1 && silverMineral2X != -1) {
+                                if (goldMineralX < silverMineral1X && goldMineralX < silverMineral2X) {
+                                    gold_mineral_position = "left";
+                                } else if (goldMineralX > silverMineral1X && goldMineralX > silverMineral2X) {
+                                    gold_mineral_position = "right";
+                                } else {
+                                    gold_mineral_position = "center";
+                                }
+                            }
+                        }
+
+                        //for every recognized object
+                        for (Recognition recognition : updatedRecognitions) {
+                            //if one of the recognized objects is the gold mineral
+                            if (recognition.getLabel().equals(LABEL_GOLD_MINERAL)) {
                                 left_coordinate = (int) recognition.getLeft();
                                 screen_width = recognition.getImageWidth();
                                 width = (int) recognition.getWidth();
                                 // define left boundary at half the screen size - half the width of the object
                                 // if left coordinate equals boundary, object is horizontally centered
-                                left_boundary = (screen_width - width)/2;
+                                left_boundary = (screen_width - width) / 2;
 
                                 telemetry.addData("# Left", left_coordinate);
                                 telemetry.addData("# Screen width", screen_width);
                                 telemetry.addData("# Width", width);
                                 telemetry.addData("# Boundary", left_boundary);
+
                                 //difference between the left coordinate and boundary in pixels
                                 distance_to_boundary = (left_boundary - left_coordinate);
-                                telemetry.addData("#", power_center);
 
-                                // if on left side of screen
-                                if (left_coordinate > left_boundary) {
-                                    //distance between object left coordinate to left boundary compared to the total left boundary size as decimal percentage
-                                    //width added because right side + half width instead of right side - half witdh
 
-                                    power_center = (double)distance_to_boundary/(left_boundary+width);
+                                // if on right half of screen
+                                if (left_coordinate < left_boundary) {
+                                    //the decimal percentage of distance-to-boundary of the edge-to-boundary size in pixels
+                                    //left_boundary already expressed as the pixels from left edge
+                                    power_center = (double) distance_to_boundary / left_boundary;
+                                    telemetry.addData("#", "Drive left");
+                                    telemetry.addData("#", power_center);
+                                }
+                                //if on left half of screen
+                                else if (left_coordinate > left_boundary) {
+                                    //the decimal percentage of distance-to-boundary of the edge-to-boundary size in pixels
+                                    //left_boundary + width is equivalent to (screen_width + width) / 2;
+                                    power_center = (double) distance_to_boundary / (left_boundary + width);
                                     telemetry.addData("#", "Drive right");
 
                                 }
-                                // if on right side of screen
-                                else if (left_coordinate < left_boundary) {
-                                    //distance between object left coordinate to left boundary compared to the total left boundary size as decimal percentage
-                                    power_center = (double)distance_to_boundary/left_boundary;
-                                    telemetry.addData("#", "Drive left");
-                                    telemetry.addData("#", power_center);
-
-                                }
+                                //in middle
+                                //TODO: increase boundary to leave +- width to count as 'in center'
                                 else {
-//                                    motor_center.setPower(0);
+                                    motor_center.setPower(0);
+                                    //go to another phase
                                 }
-//                                motor_center.setPower(power_center);
+                                motor_center.setPower(power_center);
                                 telemetry.addData("#", power_center);
 
                             }
+                            else {
+                                //look around if nothing happens?
+                                motor_center.setPower(0);
 
-                        }
-                        else {
-//                            motor_center.setPower(0);
-
+                            }
                         }
                         telemetry.update();
                     }
