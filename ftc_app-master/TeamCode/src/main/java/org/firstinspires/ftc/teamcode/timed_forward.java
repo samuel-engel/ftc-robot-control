d66c6a5f1b2ca24fde87b14d90f2d8726bba8d20
Samuel Engel <samuel-engel@outlook.com>
2019-03-07 12:09:51 +0100

    add turning ability

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/timed_forward.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/timed_forward.java
@@ -42,13 +42,23 @@ public class timed_forward extends LinearOpMode {
                 telemetry.addData("Path", "forward %2.5f S Elapsed", runtime.seconds());
                 telemetry.update();
             }
-//            runtime.reset();
+
 //            motor_left.setPower(-0.5);
 //            motor_right.setPower(-0.5);
+//            runtime.reset();
 //            while (opModeIsActive() && (runtime.seconds() < 1.0) && !interupt) {
 //                telemetry.addData("Path", "backward %2.5f S Elapsed", runtime.seconds());
 //                telemetry.update();
 //            }
+
+//            motor_left.setPower(0.5);
+//            motor_right.setPower(-0.5);
+//            runtime.reset();
+//            while (opModeIsActive() && (runtime.seconds() < 1.0) && !interupt) {
+//                telemetry.addData("Path", "sideways %2.5f S Elapsed", runtime.seconds());
+//                telemetry.update();
+//            }
+
             motor_right.setPower(0);
             motor_left.setPower(0);
             telemetry.update();
